a3ff0e0a3855d5e9b8fe001098452f0c0105b321
kiweeter123 <118579849+kiweeter123@users.noreply.github.com>
2023-11-13 11:45:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (typeof document.dev_env != "undefined") {
 }
 else {
   //get resources off of github to not inflate the jsdelivr stats
-  base_url = "https://raw.githubusercontent.com/ading2210/edpuzzle-answers/main";
+  base_url = "https://raw.githubusercontent.com/kiweeter123/edpuzzle-answers/main";
 }
 
 function http_get(url, callback, headers=[], method="GET", content=null) {
